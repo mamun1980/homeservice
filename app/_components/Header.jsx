553809025b063c9dc5daaa7a6e8b7e171781a1d3
Khondoker Md Mamunur Rashid <mamun1980@gmail.com>
2024-03-31 12:00:58 +0600

    Added signin/signup feature

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,8 +1,26 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+
+  
 
 const Header = () => {
+    const {data} = useSession()
+
+    useEffect(() => {
+        console.log(data);
+    }, [data])
+
     return (
         <div className='flex p-5 shadow-sm justify-between'>
             <div className='flex items-center gap-8'>
@@ -14,7 +32,29 @@ const Header = () => {
                 </div>
             </div>
             <div>
-                <Button>Get Started</Button>
+                {data?.user? 
+                    
+                    <DropdownMenu>
+                        <DropdownMenuTrigger  asChild>
+                        <Image src={data.user?.image} 
+                            width={40}
+                            height={40}
+                            alt='user'
+                            className='rounded-full'
+                            />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>My Booking</DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => signOut()}>Logout</DropdownMenuItem>
+                            
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    :
+                    <Button onClick={() => signIn('descope')}>Login / Sign Up</Button>
+                }
             </div>
         </div>
     )
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import NextAuthSessionProvider from './provider'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <NextAuthSessionProvider>
         <div className="mx-6 md:mx-16">
           <Header />
           {children}
         </div>
-        
+        </NextAuthSessionProvider>
       </body>
     </html>
   );
